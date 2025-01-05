c58b4e16d5dfe7e41cbb89d8682f695b03e27a9c
9mz1 <muzyyyyyyy@gmail.com>
2025-01-05 19:06:28 +0400

    Added a function to create a grid

---
+++ app.js
@@ -0,0 +1,36 @@
+// Variables
+const gridContainer = document.querySelector('.grid-container');
+
+let n = 5;
+let rows = n;
+let columns = n;
+let widthAndHeight = n * 30
+
+//Change hegiht and width of grid according to the number of boxes
+gridContainer.style.width = `${widthAndHeight}px`;
+gridContainer.style.height = `${widthAndHeight}px`;
+
+function gridSize() {
+    n = prompt("Enter a number between 1 and 100");
+    if (n >=1 && n <= 100) {
+        generateGrid()
+    } else {
+        alert("Enter a number between 1 and 100");
+    }
+}
+
+function generateGrid() {
+    gridContainer.innerHTML = "";
+    //Change hegiht and width of grid according to the number of boxes
+    let widthAndHeight = n * 30
+    gridContainer.style.width = `${widthAndHeight}px`;
+    gridContainer.style.height = `${widthAndHeight}px`;
+
+    for (let i = 0; i < n * n; i++) {
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('gridItem');
+        gridContainer.appendChild(gridItem);
+    }
+}
+
+gridSize();
